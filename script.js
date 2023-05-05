ef8f3ff01af23e08b7c25f7aa802d274e060c21d
Jakub Kodytek <jakub.kodytek@email.cz>
2023-05-05 19:23:19 +0200

    Implementace js, final

---
+++ script.js
@@ -0,0 +1,55 @@
+function addComment() {
+    var name = document.getElementById('name').value;
+    var comment = document.getElementById('comment').value;
+    
+    // Vytvoření nového komentáře
+    var newComment = {'name': name, 'comment': comment};
+
+    // Načtení existujících komentářů z localStorage
+    var comments = JSON.parse(localStorage.getItem('comments')) || [];
+    
+    // Přidání nového komentáře do pole
+    comments.push(newComment);
+    
+    // Uložení pole komentářů zpět do localStorage
+    localStorage.setItem('comments', JSON.stringify(comments));
+    
+    // Vymazání obsahu polí pro jméno a komentář
+    document.getElementById('name').value = '';
+    document.getElementById('comment').value = '';
+
+    // Po odeslání refreshnout komentáře
+    showComments();
+}
+
+function showComments() {
+    // Načtení localStorage
+    var comments = JSON.parse(localStorage.getItem('comments')) || [];
+    var commentsHtml = '';
+    
+    // Vložení komentářů do kontejneru
+    for (var i = 0; i < comments.length; i++) {
+      commentsHtml += '<div class="userComment">';
+      commentsHtml += '<p class="commentAuthor"><b>Autor:</b> ' + comments[i].name + '</p>';
+      commentsHtml += '<p class="commentContext">- <i>' + comments[i].comment + '</i></p>';
+      commentsHtml += '</div>';
+    }
+    
+    document.getElementById('comments').innerHTML = commentsHtml;
+}
+
+// Funkce pro smazání komentářů
+function removeMessages() {
+    var readyToDelete = window.prompt("Pro smazání komentářů uhádni tajnou myškavěc");
+    if(readyToDelete == "delete") {
+        localStorage.clear();
+        showComments();
+    } else {
+        window.alert("To není tajná myškavěc... takže smůla, komentáře nesmažu");
+    }
+}
+
+// Při načtení stránky se volá funkce na zobrazení komentářů
+window.onload = function(){
+    showComments();
+}
